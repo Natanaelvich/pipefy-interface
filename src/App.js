d2969d89fed25a47b6db62d861ce60d51d3d059c
natanaelvich <taelima1997@gmail.com>
2020-03-20 12:10:05 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import GlobalStyled from './styles/global';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <GlobalStyled />;
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <h1>Rocketseat</h1>
+    </Container>
+  );
+}
